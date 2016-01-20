3a0e50e9e7be3821538c552df1b66b7a927c24fb
Blomme, Maarten <Maarten.Blomme@flir.com>
2016-01-20 12:55:03 +0100

    Fixes for payment

--- app/paymentmodels.py
+++ app/paymentmodels.py
@@ -5,7 +5,7 @@ class Payment(db.Model):
     __tablename__ = 'Payments'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), unique=False)
-    type = db.Column(db.Enum(u'reservation', u'payment', u'custom'), index=True, nullable=False)
+    type = db.Column(db.Enum(u'reservation', u'membership', u'custom'), index=True, nullable=False)
     method = db.Column(db.Enum(u'terminal', u'cash', u'online'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
     reservation_id = db.Column(db.Integer, db.ForeignKey('Reservations.id'))
