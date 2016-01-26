1f6ad6b09331a5eac6afaad3c2a9178e0b12e64a
Blomme, Maarten <Maarten.Blomme@flir.com>
2016-01-26 13:37:01 +0100

    Add anonymous payments

--- app/paymentmodels.py
+++ app/paymentmodels.py
@@ -5,7 +5,7 @@ class Payment(db.Model):
     __tablename__ = 'Payments'
     id = db.Column(db.Integer, primary_key=True)
     method = db.Column(db.Enum(u'terminal', u'cash', u'online'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True)
     amount = db.Column(db.Float)
     date = db.Column(db.DateTime(), default=datetime.utcnow)
     operator_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
@@ -16,7 +16,7 @@ class PaymentDescription(db.Model):
     __tablename__ = 'PaymentDescriptions'
     id = db.Column(db.Integer, primary_key=True)
     payment_id = db.Column(db.Integer, db.ForeignKey('Payments.id'), nullable=False)
-    type = db.Column(db.Enum(u'reservation', u'membership', u'custom'), index=True, nullable=False) #Sync with payments.html
+    type = db.Column(db.Enum(u'reservation', u'membership', u'custom'), index=True, nullable=False) #Sync with pay.html
     description = db.Column(db.String(100), unique=False)
     reservation_id = db.Column(db.Integer, db.ForeignKey('Reservations.id'), nullable=True)
     amount = db.Column(db.Float)
