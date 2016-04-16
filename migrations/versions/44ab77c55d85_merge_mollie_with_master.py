e63338cb5490a73ed920e60b5ccedebf6381ee4a
Maarten Blomme <maartenblomme@gmail.com>
2016-04-16 18:34:42 +0200

    Merge db versions

---
+++ migrations/versions/44ab77c55d85_merge_mollie_with_master.py
@@ -0,0 +1,22 @@
+"""merge mollie with master
+
+Revision ID: 44ab77c55d85
+Revises: ('2a2811c50158', 'fa71f17f3182')
+Create Date: 2016-04-16 18:33:51.139865
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '44ab77c55d85'
+down_revision = ('2a2811c50158', 'fa71f17f3182')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
